9eaa75a974a5cee3ff3dc587d0616b08efb604f1
ping-n <59724381+ping-n@users.noreply.github.com>
2020-07-07 22:20:45 +1000

    card added to render user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import User from "./components/User";
+import User from "./User";
+import "bulma/css/bulma.css"
 
 export default class App extends Component {
   state = { user: null };
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -8,12 +8,37 @@ const User = (props) => {
     console.log(user);
     return (
       <>
-        <h1>Random User</h1>
-        <img src={user.picture.large} alt=""/>
-        <p>Name: {`${user.name.title}. ${user.name.first} ${user.name.last}`}</p>
-        <p></p>
+        <h1 className="title">Random User</h1>
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-128x128">
+              <img src={user.picture.large} alt={`${user.name}'s photo`} />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-content">
+                <p className="title is-4">
+                  Name: {`${user.name.first} ${user.name.last}`}
+                </p>
+                <p className="subtitle is-6">@{user.login.username}</p>
+              </div>
+            </div>
+            <div className="content">
+              <p>
+                Address:{" "}
+                {`${user.location.street.number} ${user.location.street.name}`}
+              </p>
+              <p>City: {user.location.city} </p>
+              <p>
+                State: {user.location.state} | Postcode:{" "}
+                {user.location.postcode}
+              </p>
+            </div>
+          </div>
+        </div>
       </>
-    )
+    );
   }
 };
 
